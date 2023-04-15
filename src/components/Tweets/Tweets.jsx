dfcece399909680fd477a3a3f852daf40404e5cf
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-04-15 21:57:19 +0300

    Summary

--- src/components/Tweets/Tweets.jsx
+++ src/components/Tweets/Tweets.jsx
@@ -1,18 +1,3 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import { UserList } from '../UserList/UserList';
-// import css from './Tweets.module.css';
-
-// export const Tweets = () => {
-//   return (
-//     <div>
-//           <Link className={css.link } to="/">Back</Link>
-//       <h1 className={css.title}>Сторінка з твітами</h1>
-//       <UserList />
-//     </div>
-//   );
-// };
-
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { UserList } from '../UserList/UserList';
