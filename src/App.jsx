e7f758c199ef26ce12db3a14cca407c93cdd7666
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-04-15 22:16:41 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './components/Home/Home';
 import { Tweets } from './components/Tweets/Tweets';
 
@@ -12,9 +7,9 @@ export const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/tweets" element={<Tweets />} />
-        <Route path="*" element={<Navigate to="/home" replace />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="tweets" element={<Tweets />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </Router>
   );
