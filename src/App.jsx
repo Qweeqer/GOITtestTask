f4228a83039e716f4594fab4a0a0b92ef0063842
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-04-20 17:20:02 +0300

    Додано лоудер та ліниве завантаження сторінки

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import { Loader } from './components/Loader/Loader';
+import React, { lazy, Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
 } from 'react-router-dom';
-import { Home } from './components/Home/Home';
-import { Tweets } from './components/Tweets/Tweets';
+
+// Ліниве завантаження сторінок
+const Home = lazy(() =>
+  import('./components/Home/Home').then(module => ({ default: module.Home }))
+);
+const Tweets = lazy(() =>
+  import('./components/Tweets/Tweets').then(module => ({
+    default: module.Tweets,
+  }))
+);
 
 export const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/GOITtestTask/" element={<Home />} />
-        <Route path="/GOITtestTask/tweets" element={<Tweets />} />
-        <Route path="*" element={<Navigate to="/GOITtestTask/" />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/GOITtestTask/" element={<Home />} />
+          <Route path="/GOITtestTask/tweets" element={<Tweets />} />
+          <Route path="*" element={<Navigate to="/GOITtestTask/" />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
